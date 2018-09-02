ca55413762e0afb0fa95fe516a7e0916e9b8aa67
Danielle Golinsky <degolinsky@icloud.com>
2018-09-02 18:20:29 -0400

    Added classes for storing results and sfw ratings, as well as outputting those results into strings.

---
+++ src/googleVisionTesting/Ratings.java
@@ -0,0 +1,28 @@
+package googleVisionTesting;
+
+public class Ratings {
+
+	/**
+	 * Likeliness values are Unknown, Very Unlikely, Unlikely, Possible, Likely, and Very Likely
+	 * Unknown = -1, Very likely = 4
+	 * @author Danielle Golinsky
+	 */
+	enum Likliness {
+		UNKNOWN(-1),
+		VERY_UNLIKELY(0),
+		UNLIKELY(1),
+		POSSIBLE(2),
+		LIKELY(3),
+		VERY_LIKELY(4);
+		
+		private int value = -1;
+		
+		public int getValue() {
+			return this.value;
+		}
+		
+		Likliness(int lv) {
+			this.value = lv;
+		}
+	}
+}
